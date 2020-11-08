37ee814d5b6f210e98e39c4d9069cfab1b645fee
Tomer Doar <tomerdoar@hotmail.com>
2020-11-08 03:26:53 -0500

    main: Adding red color to the timer when time is short

--- src/components/form/Timer.js
+++ src/components/form/Timer.js
@@ -2,54 +2,65 @@ import React, { useState, useEffect } from "react";
 import "./Timer.css";
 
 export default function Timer(props) {
-  const { counter } = props;
-
-  const [count, setCount] = useState(counter);
-  useEffect(() => {
-    let ccccc;
-    ccccc = setInterval(() => {
-      if (count > 0) {
-        setCount(count - 1);
-      }
-    }, 1000);
-
-    return () => {
-      clearTimeout(ccccc);
-    };
-  });
-
-  useEffect(() => {
-    setCount(counter);
-  }, [props.questionIterator, counter]);
-
-  return <div>{`Timer: ${count}`}</div>;
-
   // const { counter } = props;
 
   // const [count, setCount] = useState(counter);
-
   // useEffect(() => {
   //   let ccccc;
-  //   const runCounter = () => {
-  //     ccccc = setTimeout(() => {
+  //   ccccc = setInterval(() => {
+  //     if (count > 0) {
   //       setCount(count - 1);
-  //     }, 1000);
-  //   };
+  //     } else {
+  //       setCount(counter);
+  //       props.handleAnswerAfterTimeIsOut();
+  //     }
+  //   }, 1000);
 
-  //   if (count > 0) {
-  //     runCounter();
-  //   } else {
-  //     setCount(counter);
-  //     props.handleAnswerAfterTimeIsOut();
-  //   }
   //   return () => {
   //     clearTimeout(ccccc);
   //   };
-  // }, [count, props, counter]);
+  // });
 
   // useEffect(() => {
   //   setCount(counter);
   // }, [props.questionIterator, counter]);
 
-  // return <div>{`Timer: ${count}`}</div>;
+  // return (
+  // <div
+  //   className={`${count < 5 ? "short-count" : ""}`}
+  // >{`Timer: ${count}`}</div>
+  // );
+
+  const { counter } = props;
+
+  const [count, setCount] = useState(counter);
+
+  useEffect(() => {
+    let ccccc;
+    const runCounter = () => {
+      ccccc = setTimeout(() => {
+        setCount(count - 1);
+      }, 1000);
+    };
+
+    if (count > 0) {
+      runCounter();
+    } else {
+      setCount(counter);
+      props.handleAnswerAfterTimeIsOut();
+    }
+    return () => {
+      clearTimeout(ccccc);
+    };
+  }, [count, props, counter]);
+
+  useEffect(() => {
+    setCount(counter);
+  }, [props.questionIterator, counter]);
+
+  return (
+    <div
+      className={`${count < 5 ? "short-count" : ""}`}
+    >{`Timer: ${count}`}</div>
+  );
 }
